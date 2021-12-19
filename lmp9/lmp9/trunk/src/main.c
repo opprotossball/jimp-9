67048fdf1d970703913ee76b1834f89b22879676
opprotossball <jan.stachurski@outlook.com>
2021-12-19 15:57:59 +0100

    Podstawianie wsteczne
    Implementacja podstawiania wstecznego w backsubst.c. Dodanie do main.c obsługi błędów zgłaszanych przez backsubst.c.

--- lmp9/lmp9/trunk/src/main.c
+++ lmp9/lmp9/trunk/src/main.c
@@ -20,11 +20,15 @@ int main(int argc, char ** argv) {
 	x = createMatrix(b->r, 1);
 	if (x != NULL) {
 		res = backsubst(x,A,b);
-
-		printToScreen(x);
-	  freeMatrix(x);
+		if (res == 2)
+			fprintf(stderr,"Błąd! Nieprawidłowe rozmiary macierzy.\n");
+		else if (res == 1)
+			fprintf(stderr,"Błąd! Dzielenie przez 0.\n");
+		else
+			printToScreen(x);
+      	 	freeMatrix(x);
 	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+		fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
 	}
 
 	freeMatrix(A);
